75502771a3267f68ddcdc9409935b13d551faa6e
Neil Brittliff <nebrittl@microsoft.com>
2019-07-26 08:39:50 +1000

    Updated startup

--- startup.py
+++ startup.py
@@ -12,8 +12,7 @@ startup:app in the Gunicorn command.
 from os import environ
 from azure_trans_webapp.webapp import app
 
-print('Starting', __name__)
-
+print('Starting', __na
 if __name__ == '__main__':
 
     HOST = environ.get('SERVER_HOST', 'localhost')
